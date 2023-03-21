6c7ff6b04f2af046a0f7ba33d2486fda2d37d7a8
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-21 18:36:42 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,28 @@
 <template>
   <section>
+    <h1>ori and ori are the best</h1>
     <AppHeader />
-    <RouterView/>
-    <UserMsg/>
+    <RouterView />
+    <UserMsg />
   </section>
 </template>
 
 <script>
-
-
 import { userService } from './services/user.service'
 import { store } from './store/store'
 
 import AppHeader from './cmps/AppHeader.vue'
 import UserMsg from './cmps/UserMsg.vue'
 
-
 export default {
-
   created() {
     console.log('Vue App created')
     const user = userService.getLoggedinUser()
-    if (user)  store.commit({type: 'setLoggedinUser', user})
+    if (user) store.commit({ type: 'setLoggedinUser', user })
   },
   components: {
     AppHeader,
-    UserMsg
+    UserMsg,
   },
 }
 </script>
