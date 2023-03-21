cef3e87d2c922b4d2096851a4f54aa0dad6b8bc8
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-21 19:10:30 +0200

    added create empty functions for tasks groups and boards

--- src/services/board.service.local.js
+++ src/services/board.service.local.js
@@ -1,77 +1,115 @@
-
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 import { userService } from './user.service.js'
 
 const STORAGE_KEY = 'board'
-
+_createBoard()
 export const boardService = {
-    query,
-    getById,
-    save,
-    remove,
-    getEmptyBoard,
-    addBoardMsg
+  query,
+  getById,
+  save,
+  remove,
+  getEmptyBoard,
+  addBoardMsg,
 }
 window.cs = boardService
 
-
 async function query(filterBy = { txt: '', price: 0 }) {
-    var boards = await storageService.query(STORAGE_KEY)
-    if (filterBy.txt) {
-        const regex = new RegExp(filterBy.txt, 'i')
-        boards = boards.filter(board => regex.test(board.vendor) || regex.test(board.description))
-    }
-    if (filterBy.price) {
-        boards = boards.filter(board => board.price <= filterBy.price)
-    }
-    return boards
+  var boards = await storageService.query(STORAGE_KEY)
+  if (filterBy.txt) {
+    const regex = new RegExp(filterBy.txt, 'i')
+    boards = boards.filter(
+      (board) => regex.test(board.vendor) || regex.test(board.description)
+    )
+  }
+  if (filterBy.price) {
+    boards = boards.filter((board) => board.price <= filterBy.price)
+  }
+  return boards
 }
 
 function getById(boardId) {
-    return storageService.get(STORAGE_KEY, boardId)
+  return storageService.get(STORAGE_KEY, boardId)
 }
 
 async function remove(boardId) {
-    await storageService.remove(STORAGE_KEY, boardId)
+  await storageService.remove(STORAGE_KEY, boardId)
 }
 
 async function save(board) {
-    var savedBoard
-    if (board._id) {
-        savedBoard = await storageService.put(STORAGE_KEY, board)
-    } else {
-        // Later, owner is set by the backend
-        board.owner = userService.getLoggedinUser()
-        savedBoard = await storageService.post(STORAGE_KEY, board)
-    }
-    return savedBoard
+  var savedBoard
+  if (board._id) {
+    savedBoard = await storageService.put(STORAGE_KEY, board)
+  } else {
+    // Later, owner is set by the backend
+    board.owner = userService.getLoggedinUser()
+    savedBoard = await storageService.post(STORAGE_KEY, board)
+  }
+  return savedBoard
 }
 
 async function addBoardMsg(boardId, txt) {
-    // Later, this is all done by the backend
-    const board = await getById(boardId)
-    if (!board.msgs) board.msgs = []
+  // Later, this is all done by the backend
+  const board = await getById(boardId)
+  if (!board.msgs) board.msgs = []
 
-    const msg = {
-        id: utilService.makeId(),
-        by: userService.getLoggedinUser(),
-        txt
-    }
-    board.msgs.push(msg)
-    await storageService.put(STORAGE_KEY, board)
+  const msg = {
+    id: utilService.makeId(),
+    by: userService.getLoggedinUser(),
+    txt,
+  }
+  board.msgs.push(msg)
+  await storageService.put(STORAGE_KEY, board)
 
-    return msg
+  return msg
 }
 
-function getEmptyBoard() {
-    return {
-        vendor: 'Susita-' + (Date.now() % 1000),
-        price: utilService.getRandomIntInclusive(1000, 9000),
-    }
+function getEmptyBoard(
+  title = '',
+  isStarred = false,
+  labels = [],
+  createdBy = {},
+  groups = [getEmptyGroup()]
+) {
+  return {
+    _id: '',
+    title,
+    isStarred,
+    labels,
+    createdBy,
+    groups,
+  }
 }
 
+function getEmptyGroup(title = '', archivedAt = null, tasks = [], style = {}) {
+  return {
+    id: '',
+    title: '',
+    archivedAt: null,
+    tasks: [getEmptyTask()],
+    style: {},
+  }
+}
 
+function getEmptyTask(
+  title = '',
+  status = '',
+  priority = '',
+  description = ''
+) {
+  return {
+    id: '',
+    title: '',
+    status: '',
+    priority: '',
+    description: '',
+  }
+}
+
+function _createBoard() {
+  let res = getEmptyBoard()
+  console.log(res)
+}
 // TEST DATA
 // ;(async ()=>{
 //     await storageService.post(STORAGE_KEY, {vendor: 'Subali Karov 1', price: 180})
--- src/services/util.service.js
+++ src/services/util.service.js
@@ -1,63 +1,116 @@
 export const utilService = {
-    makeId,
-    makeLorem,
-    getRandomIntInclusive,
-    debounce,
-    randomPastTime,
-    saveToStorage,
-    loadFromStorage
+  makeId,
+  makeLorem,
+  getRandomIntInclusive,
+  debounce,
+  randomPastTime,
+  saveToStorage,
+  loadFromStorage,
+  getRandomTasks,
 }
 
 function makeId(length = 6) {
-    var txt = ''
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var txt = ''
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
-    for (var i = 0; i < length; i++) {
-        txt += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
 
-    return txt
+  return txt
 }
 
 function makeLorem(size = 100) {
-    var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
-    var txt = ''
-    while (size > 0) {
-        size--
-        txt += words[Math.floor(Math.random() * words.length)] + ' '
-    }
-    return txt
+  var words = [
+    'The sky',
+    'above',
+    'the port',
+    'was',
+    'the color of television',
+    'tuned',
+    'to',
+    'a dead channel',
+    '.',
+    'All',
+    'this happened',
+    'more or less',
+    '.',
+    'I',
+    'had',
+    'the story',
+    'bit by bit',
+    'from various people',
+    'and',
+    'as generally',
+    'happens',
+    'in such cases',
+    'each time',
+    'it',
+    'was',
+    'a different story',
+    '.',
+    'It',
+    'was',
+    'a pleasure',
+    'to',
+    'burn',
+  ]
+  var txt = ''
+  while (size > 0) {
+    size--
+    txt += words[Math.floor(Math.random() * words.length)] + ' '
+  }
+  return txt
 }
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive
 }
-
 
 function randomPastTime() {
-    const HOUR = 1000 * 60 * 60
-    const DAY = 1000 * 60 * 60 * 24
-    const WEEK = 1000 * 60 * 60 * 24 * 7
+  const HOUR = 1000 * 60 * 60
+  const DAY = 1000 * 60 * 60 * 24
+  const WEEK = 1000 * 60 * 60 * 24 * 7
 
-    const pastTime = getRandomIntInclusive(HOUR, WEEK)
-    return Date.now() - pastTime
+  const pastTime = getRandomIntInclusive(HOUR, WEEK)
+  return Date.now() - pastTime
 }
 
-function debounce(func, timeout = 300){
-    let timer
-    return (...args) => {
-      clearTimeout(timer)
-      timer = setTimeout(() => { func.apply(this, args) }, timeout)
-    }
+function debounce(func, timeout = 300) {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, timeout)
+  }
 }
 
 function saveToStorage(key, value) {
-    localStorage.setItem(key, JSON.stringify(value))
+  localStorage.setItem(key, JSON.stringify(value))
 }
 
 function loadFromStorage(key) {
-    const data = localStorage.getItem(key)
-    return (data) ? JSON.parse(data) : undefined
+  const data = localStorage.getItem(key)
+  return data ? JSON.parse(data) : undefined
+}
+
+function getRandomTasks(idx = getRandomIntInclusive(0, 9)) {
+  const codingTasks = [
+    'Implement a function to check whether a given string is a palindrome or not.',
+    'Create a program to find the largest number in an array of numbers.',
+    'Write a function that takes an array of integers and returns the sum of all the even numbers.',
+    'Implement a function that converts a string to title case (the first letter of each word is capitalized).',
+    'Create a program to sort an array of objects by a specific property.',
+    'Write a function to remove all duplicates from an array of strings.',
+    'Implement a function to check if a given string contains only unique characters.',
+    'Create a program to calculate the factorial of a given number.',
+    'Write a function that finds the second smallest element in an array of integers.',
+    'Implement a function to reverse a string without using the built-in reverse() method.',
+  ]
+
+  return codingTasks[idx]
 }
