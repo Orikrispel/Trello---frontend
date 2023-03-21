a45259275ccc3440fe35e076f468ef334683a808
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-21 19:32:50 +0200

    added demo data

--- src/services/board.service.local.js
+++ src/services/board.service.local.js
@@ -3,7 +3,7 @@ import { utilService } from './util.service.js'
 import { userService } from './user.service.js'
 
 const STORAGE_KEY = 'board'
-_createBoard()
+// _createBoard()
 export const boardService = {
   query,
   getById,
@@ -16,6 +16,10 @@ window.cs = boardService
 
 async function query(filterBy = { txt: '', price: 0 }) {
   var boards = await storageService.query(STORAGE_KEY)
+  //   if (!boards || !boards.length) {
+  //     _createBoard()
+  //   }
+
   if (filterBy.txt) {
     const regex = new RegExp(filterBy.txt, 'i')
     boards = boards.filter(
@@ -106,9 +110,134 @@ function getEmptyTask(
   }
 }
 
-function _createBoard() {
-  let res = getEmptyBoard()
-  console.log(res)
+async function _createBoard() {
+  let board = {
+    _id: '',
+    title: 'Robot dev proj',
+    isStarred: false,
+    archivedAt: 1589983468418,
+    createdBy: {
+      _id: 'u101',
+      fullname: 'Abi Abambi',
+      imgUrl: 'http://some-img',
+    },
+    style: {},
+    labels: [
+      {
+        id: 'l101',
+        title: 'Done',
+        color: '#61bd4f',
+      },
+      {
+        id: 'l102',
+        title: 'Progress',
+        color: '#61bd33',
+      },
+    ],
+    members: [
+      {
+        _id: 'u101',
+        fullname: 'Tal Tarablus',
+        imgUrl: 'https://www.google.com',
+      },
+    ],
+    groups: [
+      {
+        id: 'g101',
+        title: 'Group 1',
+        archivedAt: 1589983468418,
+        tasks: [
+          {
+            id: 'c101',
+            title: 'Replace logo',
+          },
+          {
+            id: 'c102',
+            title: 'Add Samples',
+          },
+        ],
+        style: {},
+      },
+      {
+        id: 'g102',
+        title: 'Group 2',
+        tasks: [
+          {
+            id: 'c103',
+            title: 'Do that',
+            archivedAt: 1589983468418,
+          },
+          {
+            id: 'c104',
+            title: 'Help me',
+            status: 'in-progress', // monday
+            priority: 'high',
+            description: 'description',
+            comments: [
+              {
+                id: 'ZdPnm',
+                txt: 'also @yaronb please CR this',
+                createdAt: 1590999817436,
+                byMember: {
+                  _id: 'u101',
+                  fullname: 'Tal Tarablus',
+                  imgUrl:
+                    'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+                },
+              },
+            ],
+            checklists: [
+              {
+                id: 'YEhmF',
+                title: 'Checklist',
+                todos: [
+                  {
+                    id: '212jX',
+                    title: 'To Do 1',
+                    isDone: false,
+                  },
+                ],
+              },
+            ],
+            memberIds: ['u101'],
+            labelIds: ['l101', 'l102'],
+            dueDate: 16156215211,
+            byMember: {
+              _id: 'u101',
+              username: 'Tal',
+              fullname: 'Tal Tarablus',
+              imgUrl:
+                'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+            },
+            style: {
+              bgColor: '#26de81',
+            },
+          },
+        ],
+        style: {},
+      },
+    ],
+    activities: [
+      {
+        id: 'a101',
+        txt: 'Changed Color',
+        createdAt: 154514,
+        byMember: {
+          _id: 'u101',
+          fullname: 'Abi Abambi',
+          imgUrl: 'http://some-img',
+        },
+        task: {
+          id: 'c101',
+          title: 'Replace Logo',
+        },
+      },
+    ],
+
+    cmpsOrder: ['status-picker', 'member-picker', 'date-picker'],
+  }
+  board = await save(board)
+  return board
 }
 // TEST DATA
 // ;(async ()=>{
--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -10,16 +10,14 @@ export const SOCKET_EVENT_USER_UPDATED = 'user-updated'
 const SOCKET_EMIT_LOGIN = 'set-user-socket'
 const SOCKET_EMIT_LOGOUT = 'unset-user-socket'
 
-
-const baseUrl = (process.env.NODE_ENV === 'production') ? '' : '//localhost:3030'
-export const socketService = createSocketService()
-// export const socketService = createDummySocketService()
+const baseUrl = process.env.NODE_ENV === 'production' ? '' : '//localhost:3030'
+// export const socketService = createSocketService()
+export const socketService = createDummySocketService()
 
 // for debugging from console
 window.socketService = socketService
 
 socketService.setup()
-
 
 function createSocketService() {
   var socket = null
@@ -32,7 +30,7 @@ function createSocketService() {
       }, 500)
     },
     on(eventName, cb) {
-      console.log(socket);
+      console.log(socket)
       socket.on(eventName, cb)
     },
     off(eventName, cb = null) {
@@ -53,7 +51,6 @@ function createSocketService() {
     terminate() {
       socket = null
     },
-
   }
   return socketService
 }
@@ -69,17 +66,18 @@ function createDummySocketService() {
     terminate() {
       this.setup()
     },
-    login() {
-    },
-    logout() {
-    },
+    login() {},
+    logout() {},
     on(eventName, cb) {
-      listenersMap[eventName] = [...(listenersMap[eventName]) || [], cb]
+      listenersMap[eventName] = [...(listenersMap[eventName] || []), cb]
     },
     off(eventName, cb) {
       if (!listenersMap[eventName]) return
       if (!cb) delete listenersMap[eventName]
-      else listenersMap[eventName] = listenersMap[eventName].filter(l => l !== cb)
+      else
+        listenersMap[eventName] = listenersMap[eventName].filter(
+          (l) => l !== cb
+        )
     },
     emit(eventName, data) {
       var listeners = listenersMap[eventName]
@@ -89,22 +87,27 @@ function createDummySocketService() {
 
       if (!listeners) return
 
-      listeners.forEach(listener => {
+      listeners.forEach((listener) => {
         listener(data)
       })
     },
     // Functions for easy testing of pushed data
     testChatMsg() {
-      this.emit(SOCKET_EVENT_ADD_MSG, { from: 'Someone', txt: 'Aha it worked!' })
+      this.emit(SOCKET_EVENT_ADD_MSG, {
+        from: 'Someone',
+        txt: 'Aha it worked!',
+      })
     },
     testUserUpdate() {
-      this.emit(SOCKET_EVENT_USER_UPDATED, { ...userService.getLoggedinUser(), score: 555 })
+      this.emit(SOCKET_EVENT_USER_UPDATED, {
+        ...userService.getLoggedinUser(),
+        score: 555,
+      })
     },
   }
   window.listenersMap = listenersMap
   return socketService
 }
-
 
 // Basic Tests
 // function cb(x) {console.log('Socket Test - Expected Puk, Actual:', x)}
