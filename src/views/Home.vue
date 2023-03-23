b7bae4198f8bf0234d49941aab8fd3ac235f72da
Teicher Ori <teicheror@mta.ac.il>
2023-03-23 11:03:49 +0200

    no changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
-  <section class="container home text-center">
-    <h1>Welcome To Trello</h1>
-    <h5 class="text-center">
-      Powered up Frontend
-    </h5>
+  <section class="homepage">
+    <div class="intro-home-container">
+      <div class="intro-header">
+        <h1>Trello brings all your tasks, teammates, and tools together
+          Keep everything in the same place—even if your team isn’t.</h1>
+        <p> Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way
+          your
+          team works is unique—accomplish it all with Trello.</p>
+      </div>
+      <img class="intro-img" src="../../src/assets/imgs/trello-homepage-1.png">
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'home',
+  name: 'homepage',
   data() {
     return {
     }
