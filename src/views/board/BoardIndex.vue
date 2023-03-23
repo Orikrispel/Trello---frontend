f23a87349c63310f478c7ca9964f4e16ca5a20d8
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-23 16:18:13 +0200

    fixed color picker in board index

--- src/views/board/BoardIndex.vue
+++ src/views/board/BoardIndex.vue
@@ -10,16 +10,12 @@
         <h5>Create Board</h5>
         <p @click="setCreateMode">x</p>
       </div>
+
       <div
         class="board-display"
         :style="{ 'background-color': boardPickedColor }"></div>
       <label for="color-picker">Background</label>
       <ColorPicker :quantity="6" @setColor="setBoardBgColor" />
-      <div
-        class="board-display"
-        :style="{ 'background-color': boardPickedColor }"></div>
-      <label for="color-picker">Background</label>
-      <ColorPicker @setColor="setBoardBgColor" />
       <label for="board-title">Board title</label>
       <input name="board-title" type="text" v-model="boardToAdd.title" />
       <button>Create</button>
