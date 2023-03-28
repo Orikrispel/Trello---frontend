249970d23223de730c5ec9465395ef12930186e4
Teicher Ori <teicheror@mta.ac.il>
2023-03-28 18:51:27 +0300

    right menu changes

---
+++ src/cmps/right-menu/ChangeBgMenu.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="change-bg-menu">
+        <div class="change-bg-item">
+            <p>photos</p>
+        </div>
+        <div class="change-bg-item">
+            <p>Colors</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ChangeBgMenu',
+    props: {
+    },
+    data() {
+        return {
+        }
+    },
+    created() {
+
+    },
+    methods: {
+
+    },
+    computed: {
+    },
+}
+</script>
---
+++ src/cmps/right-menu/RightMenuMain.vue
@@ -0,0 +1,45 @@
+<template>
+    <section class="right-menu-actions">
+        <button>
+            <span v-html="getSvg('addMember')"></span>
+            About this board
+        </button>
+        <button @click="this.$emit('ChangeCmp', 'ChangeBgMenu')">
+            <span v-html="getSvg('pencil')"></span>
+            Change background
+        </button>
+    </section>
+</template>
+
+<script>
+import { svgService } from '../../services/svg.service';
+import { eventBus } from '../../services/event-bus.service'
+export default {
+    name: 'RightMenuMain',
+    props: {
+        task: {
+            type: Object,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            currTask: null,
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        getSvg(iconName) {
+            return svgService.getSvg(iconName)
+        },
+    },
+    computed: {
+
+
+    },
+}
+</script>
+
+
