793c1cc750616208ef58510ad7ebcca2bb7e1472
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-28 21:09:14 +0300

    add formatting date string funtion to util service

--- src/cmps/dates/DatePreview.vue
+++ src/cmps/dates/DatePreview.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="due-date-preview-container">
     <input type="checkbox" />
-    <button class="btn btn-light">
+    <button @click="test" class="btn btn-light flex">
       {{ dateForDisplay }}
+      <span v-if="isOverdue" class="span-overdue">overdue </span>
+      <br />
+      <span v-if="isDueSoon" class="span-due-soon">due soon</span>
       <span class="icon icon-arrow-down" v-html="getSvg('arrowDown')"></span>
     </button>
   </div>
@@ -20,10 +23,37 @@ export default {
   },
   computed: {
     dateForDisplay() {
-      return utilService.getFormattedDate(this.date)
+      const diff = this.diff(this.date.dueDate)
+      if (diff === 0) {
+        return 'Today'
+      } else if (diff === 1) {
+        return 'Tomorrow'
+      } else if (diff === -1) {
+        return 'Yesterday'
+      } else {
+        return utilService.formatDateString(this.date.dueDate)
+      }
+    },
+    isOverdue() {
+      if (this.diff(this.date.dueDate) <= 0) return true
+      return false
+    },
+    isDueSoon() {
+      const diff = this.diff(this.date.dueDate)
+      if (diff >= -2 && diff <= 2 && diff >= 0) return true
+      return false
     },
   },
+
   methods: {
+    test() {
+      console.log(this.diff(this.date.dueDate))
+    },
+    diff(date) {
+      const millisecondsPerDay = 24 * 60 * 60 * 1000
+      const diffInMilliseconds = Math.abs(date - Date.now())
+      return Math.floor(diffInMilliseconds / millisecondsPerDay)
+    },
     getSvg(iconName) {
       return svgService.getSvg(iconName)
     },
