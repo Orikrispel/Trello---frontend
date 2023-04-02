1c43988afd9949a07fc513dcb8fb921fb933d1e2
Ori Krispel <orikrispel8@gmail.com>
2023-04-02 19:37:20 +0300

    Order in css

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -1,17 +1,11 @@
 <template>
   <header class="app-header">
     <nav class="flex gap">
-      <RouterLink class="flex" to="/"
-        ><img class="logo" src="../assets/logo white.png" alt="Jello logo"
-      /></RouterLink>
+      <RouterLink class="flex" to="/"><img class="logo" src="../assets/logo white.png" alt="Jello logo" /></RouterLink>
       <RouterLink to="/board">Boards</RouterLink>
       <button class="btn add-board" @click="isCreateMode = !isCreateMode">
         Create
-        <AddBoard
-          v-show="isCreateMode"
-          @addBoard="addBoard"
-          @closeModal="isCreateMode = false"
-          style="top: 40px" />
+        <AddBoard v-show="isCreateMode" @addBoard="addBoard" @closeModal="isCreateMode = false" style="top: 40px" />
       </button>
     </nav>
     <button @click="showUsers">users</button>
@@ -19,11 +13,7 @@
     <nav class="flex gap">
       <section class="loggedin-user" v-if="loggedinUser">
         <RouterLink class="member-wrapper flex align-center" :to="`/login`">
-          <img
-            class="user-img"
-            v-if="loggedinUser.imgUrl"
-            :src="loggedinUser.imgUrl"
-            alt="" />
+          <img class="user-img" v-if="loggedinUser.imgUrl" :src="loggedinUser.imgUrl" alt="" />
           <div v-else class="member-img">
             <span>{{ loggedinUser.fullname.charAt(0).toUpperCase() }}</span>
           </div>
@@ -43,7 +33,6 @@
 <script>
 // import { boardService } from '../services/board.service.local'
 import { boardService } from '../services/board.service'
-import { boardService } from '../services/board.service'
 import { svgService } from '../services/svg.service'
 import AddBoard from '../cmps/AddBoard.vue'
 export default {
@@ -56,7 +45,7 @@ export default {
       isCreateMode: false,
     }
   },
-  created() {},
+  created() { },
   computed: {
     loggedinUser() {
       return this.$store.getters.loggedinUser
