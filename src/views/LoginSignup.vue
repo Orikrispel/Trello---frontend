32af646138e90ae16fcf352ac20d4f80f9d3c244
Ori Krispel <orikrispel8@gmail.com>
2023-04-02 09:58:00 +0300

    login-signup improve UI

--- src/views/LoginSignup.vue
+++ src/views/LoginSignup.vue
@@ -1,26 +1,51 @@
 <template>
   <section class="login-signup main flex column align-center">
     <img class="logo" src="../assets/logo colored.png" alt="Jello logo" />
-    <article class="login-form flex column">
-      <h2>Log in to Jello</h2>
 
+    <article v-if="!loggedinUser && loginClicked" class="login-form flex column">
+      <h2>Log in to Jello</h2>
       <form @submit.prevent="login" class="flex column align-center">
         <input type="text" v-model="credentials.username" placeholder="Enter username" />
         <input type="password" v-model="credentials.password" placeholder="Enter password" />
-        <button>Log in</button>
+        <button type="submit">Log in</button>
       </form>
 
       <div class="flex column status">
         <p class="seperator">OR</p>
         <hr>
-        <p class="toggle-status">Sign up for an account</p>
+        <p class="toggle-status" @click="toggleForm">Sign up for an account</p>
       </div>
     </article>
+
+    <article v-if="!loggedinUser && signupClicked" class="login-form flex column">
+      <h2 v-if="!loggedinUser">
+        Signup to Jello
+      </h2>
+      <form @submit.prevent="login" class="flex column align-center">
+        <input type="text" v-model="signupInfo.fullname" placeholder="Full name" />
+        <input type="text" v-model="signupInfo.username" placeholder="Username" />
+        <input type="password" v-model="signupInfo.password" placeholder="Password" />
+        <button type="submit">Signup</button>
+      </form>
+
+      <div class="flex column status">
+        <p class="seperator">Already have an account?</p>
+        <hr>
+        <p class="toggle-status" @click="toggleForm">Log in</p>
+      </div>
+    </article>
+
+    <button v-if="loggedinUser" class=" btn btn-light" @click="logout">Logout</button>
+
+    <div class="img-comtainer flex justify-between">
+      <img src="../assets/imgs/loginSingup/svg1.svg" />
+      <img src="../assets/imgs/loginSingup/svg2.svg" />
+    </div>
   </section>
 
 
-  <section class="login-signup">
-    <h2 v-if="loggedinUser" class="btn-rounded" @click="logout">Logout</h2>
+  <!-- <section class="login-signup">
+
     <h2 v-if="!loggedinUser" @click="loginClicked = true" class="btn-rounded">
       Login
     </h2>
@@ -38,7 +63,7 @@
       <input type="password" v-model="signupInfo.password" placeholder="Password" />
       <button>Signup</button>
     </form>
-  </section>
+  </section> -->
 </template>
 <script>
 import {
@@ -46,10 +71,11 @@ import {
   showSuccessMsg,
   showErrorMsg,
 } from '../services/event-bus.service'
+import { svgService } from '../services/svg.service'
 export default {
   data() {
     return {
-      loginClicked: false,
+      loginClicked: true,
       signupClicked: false,
 
       credentials: {
@@ -96,6 +122,15 @@ export default {
     async logout() {
       this.$store.dispatch({ type: 'logout' })
     },
+
+    getSvg(iconName) {
+      return svgService.getSvg(iconName)
+    },
+
+    toggleForm() {
+      this.signupClicked = !this.signupClicked
+      this.loginClicked = !this.loginClicked
+    }
   },
 }
 </script>
