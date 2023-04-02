1bfeb86094e04b1911a5237ce27332466baaab4a
Ori Krispel <orikrispel8@gmail.com>
2023-04-02 13:53:29 +0300

    hompage improved UI

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -30,7 +30,8 @@ import {
   SOCKET_EVENT_ADD_MSG,
   SOCKET_EMIT_SET_TOPIC,
 } from '../services/socket.service'
-import { boardService } from '../services/board.service'
+// import { boardService } from '../services/board.service'
+import { boardService } from '../services/board.service.local'
 import { eventBus } from '../services/event-bus.service'
 import CommentList from '../cmps/comments/CommentList.vue'
 export default {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
-  <main class="homepage">
-    <section class="hero flex">
-      <div class="hero-container">
-        <div class="hero-intro">
-          <h1>Jello brings all your tasks, teammates, and tools together</h1>
-          <p>Keep everything in the same place—even if your team isn’t.</p>
-          <Router-Link @click="setDemoUser" to="/board" class="btn btn-blue">Start demo</Router-Link>
+  <section class="homepage-container">
+    <header class="homepage-header flex justify-between">
+      <div class="logo flex flex-center"><img src="../assets/logo colored.png"></div>
+      <nav class="nav-container flex">
+        <RouterLink to="/login">Log in</RouterLink>
+        <RouterLink to="/board">Start demo</RouterLink>
+      </nav>
+    </header>
+
+    <main class="homepage-main">
+      <section class="hero flex">
+        <div class="hero-container">
+          <div class="hero-intro">
+            <h1>Jello brings all your tasks, teammates, and tools together</h1>
+            <p>Keep everything in the same place — even if your team isn’t.</p>
+            <Router-Link @click="setDemoUser" to="/board" class="btn-demo fs16">Start demo</Router-Link>
+          </div>
+          <img class="intro-img" src="../../src/assets/imgs/homepage/homepage-2.png" />
         </div>
-        <img class="intro-img" src="../../src/assets/imgs/homepage/homepage-2.png" />
-      </div>
-    </section>
-  </main>
+      </section>
+    </main>
+  </section>
 </template>
 
 <script>
